93953a6058d2140a49db39419377fdbc3a0481be
Dawar13 <dawar.iitb@gmail.com>
2026-04-16 13:39:24 +0530

    posthog added

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
+import posthog from 'posthog-js'
+
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2026-01-30',
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
